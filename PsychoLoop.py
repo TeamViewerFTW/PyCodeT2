57f2f0bebbef344d4ab9dbf7faee844cda72275d
TeamViewerFTW <atticusb05@gmail.com>
2018-05-04 14:28:16 +0800

    Edited, and more

--- PsychoLoop.py
+++ PsychoLoop.py
@@ -5,10 +5,13 @@ B = random.random()
 G = random.random()
 
 turtle.pen()
+turtle.shape("train")
 while True:
+    turtle.pen(5)
+    R = random.random()
+    B = random.random()
+    G = random.random()
     turtle.color(R, G, B)
     turtle.forward(100)
     turtle.backward(200)
     turtle.forward(100)
-
-
